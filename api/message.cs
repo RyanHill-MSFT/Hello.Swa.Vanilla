c8f0d401c4882f7fb1722b45f933f038c036770a
Ryan Hill <ryanchill@hotmail.com>
2024-11-01 13:24:27 -0400

    feat: update response format to return JSON object in Azure Functions API

--- api/message.cs
+++ api/message.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -18,7 +19,10 @@ namespace Company.Function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new OkObjectResult(new JsonObject
+            {
+                { "message", "Welcome to Azure Functions!" }
+            });
         }
     }
 }
